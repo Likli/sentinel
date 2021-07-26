0dc9042773f2536ae2c363c9d6dcec98df92479d
Василий <bobit300@gmail.com>
2021-07-26 14:24:39 +0300

    fix Dockerfile

--- share/run.sh
+++ share/run.sh
@@ -35,42 +35,14 @@ if [ -n "$server" ]; then
   echo "server=${server}" >> /.hcccore/hcc.conf
 fi
 
-if [ ! -f /sentinel/sentinel.conf ]; then
-  if [ -z "$RPCHOST" ]; then
-    echo "When no /sentinel/sentinal.conf is present, you must at least set the RPCHOST environment variable"
-    exit 1
-  fi
+echo "" > /sentinel/hcc.conf
 
-  echo "dash_conf=/sentinel/hcc.conf" > /sentinel/sentinel.conf
-  if [ -n "$RPCHOST" ]; then
-    echo "rpchost=${RPCHOST}" >> /sentinel/sentinel.conf
-  fi
-  if [ -n "$NETWORK" ]; then
-    echo "network=${NETWORK}" >> /sentinel/sentinel.conf
-  fi
-
-  # If these variables are not present, we let sentinel handle default values (which will use sqlite)
-  if [ -n "$DB_HOST" ]; then
-    echo "db_host=${DB_HOST}" >> /sentinel/sentinel.conf
-  fi
-  if [ -n "$DB_PORT" ]; then
-    echo "db_port=${DB_PORT}" >> /sentinel/sentinel.conf
-  fi
-  if [ -n "$DB_NAME" ]; then
-    echo "db_name=${DB_NAME}" >> /sentinel/sentinel.conf
-  fi
-  if [ -n "$DB_USER" ]; then
-    echo "db_user=${DB_USER}" >> /sentinel/sentinel.conf
-  fi
-  if [ -n "$DB_PASSWORD" ]; then
-    echo "db_password=${DB_PASSWORD}" >> /sentinel/sentinel.conf
-  fi
-  if [ -n "$DB_CHARSET" ]; then
-    echo "db_charset=${DB_CHARSET}" >> /sentinel/sentinel.conf
-  fi
-  if [ -n "$DB_DRIVER" ]; then
-    echo "db_driver=${DB_DRIVER}" >> /sentinel/sentinel.conf
-  fi
+if [ -n "$RPCHOST" ]; then
+  echo "rpchost=${RPCHOST}" >> /sentinel/sentinel.conf
+  echo "dash_conf=/.hcccore/hcc.conf" >> /sentinel/sentinel.conf
+  echo "network=mainnet" >> /sentinel/sentinel.conf
+  echo "db_name=database/sentinel.db" >> /sentinel/sentinel.conf
+  echo "db_driver=sqlite" >> /sentinel/sentinel.conf
 fi
 
 while true; do
